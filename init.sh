c82e2a76beaa855547bc6384f0335cf049c0998b
Hao Wang <wh.sjtu@gmail.com>
2016-10-09 10:49:43 -0400

    update init.sh

--- init.sh
+++ init.sh
@@ -3,3 +3,6 @@ sudo apt-get install -y software-properties-common
 sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install -y ansible
+
+cp hosts.template > hosts
+cp group_vars/all.template > group_vars/all
