5311f4cab98b5d59a9a5f1221d892660c7d9c9dd
Hao Wang <wh.sjtu@gmail.com>
2016-10-09 10:51:53 -0400

    update init.sh

--- init.sh
+++ init.sh
@@ -3,6 +3,11 @@ sudo apt-get install -y software-properties-common
 sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install -y ansible
+sudo apt-get install -y git
+
+git clone https://github.com/chapter09/initVM.git
+
+cd initVM
 
 cp hosts.template > hosts
 cp group_vars/all.template > group_vars/all
